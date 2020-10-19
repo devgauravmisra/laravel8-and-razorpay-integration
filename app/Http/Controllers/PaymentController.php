11bce165bbde0767346174860015d96f06f4386c
Guarav Mishra <gauravphpdev@gmail.com>
2020-10-19 17:35:14 +0530

    first commit in laravel razorpay repo

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Http\controllers;
+
+use Illuminate\Http\Request;
+use Razorpay\Api\Api;
+use Session;
+use Redirect;
+use App\Http\controllers\Controller;
+
+
+class PaymentController extends Controller
+{    
+    public function create()
+    {    
+        return view('payWithRazorpay');
+    }
+
+    public function payment(Request $request)
+    {
+        $input = $request->all();
+
+        $api = new Api(env('RAZOR_KEY'), env('RAZOR_SECRET'));
+
+        $payment = $api->payment->fetch($input['razorpay_payment_id']);
+         
+        if(count($input)  && !empty($input['razorpay_payment_id'])) {
+            try {
+                $response = $api->payment->fetch($input['razorpay_payment_id'])->capture(array('amount'=>$payment['amount'])); 
+              
+                            } catch (\Exception $e) {
+                return  $e->getMessage();
+                \Session::put('error',$e->getMessage());
+                return redirect()->back();
+            }
+        }
+        
+        \Session::put('success', 'Payment successful');
+        return redirect()->back();
+    }
+}
